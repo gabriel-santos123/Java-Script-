26e891dd41996c37ba9b0a38642fc2083e0c4ebc
Gabriel <150263932+gabriel-santos123@users.noreply.github.com>
2024-04-11 17:00:37 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
-const dataAtual = new Date ();
-let dataNiver = prompt ("Data do próximo evento:");
-
-dataNiver = new Date (dataNiver+"T23:59:59");
-let diasQueFaltam = Math.floor((dataNiver-dataAtual)/86400000)
-
-document.querySelector("#dias_restantes").textcontent=diasQueFaltam;
+const DataAtual = new Date();
+let DataObjetivo = prompt("Data Escolhida:");
+DataObjetivo = new Date(DataObjetivo + "T23:59:59");
+let diasQueFaltam = Math.floor((DataObjetivo-DataAtual)/86400000);
+document.querySelector("#dias_restantes").textContent=diasQueFaltam;
