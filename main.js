662aa3c111c745a726098f3c894e6801790bb05a
Gabriel <150263932+gabriel-santos123@users.noreply.github.com>
2024-10-10 16:52:58 -0300

    d

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-const DataAtual = new Date();
-let DataObjetivo = prompt("Data Escolhida:");
-DataObjetivo = new Date(DataObjetivo + "T23:59:59");
-let diasQueFaltam = Math.floor((DataObjetivo-DataAtual)/86400000);
-document.querySelector("#dias_restantes").textContent=diasQueFaltam;
+
+document.getElementById('calcular_btn').addEventListener('click', function() {
+    const dataEvento = new Date(document.getElementById('data_evento').value);
+    const hoje = new Date();
+    
+
+    const diff = dataEvento - hoje;
+
+
+    const diasRestantes = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+    const diasRestantesElement = document.getElementById('dias_restantes');
+    
+    if (diasRestantes < 0) {
+        diasRestantesElement.textContent = 'O evento já ocorreu!';
+    } else {
+        diasRestantesElement.textContent = `${diasRestantes} dias restantes até o evento.`;
+    }
+});
